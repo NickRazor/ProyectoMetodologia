9aeba278bec3a0d17ef7a1aac9e251b96379a052
RaysonLopez <rslopez6@espe.edu.ec>
2025-01-20 23:36:41 -0500

    Initial commit

---
+++ src/main/java/com/happymarket/HappyMarketApplication.java
@@ -0,0 +1,13 @@
+package com.happymarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/happymarket/HappyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
